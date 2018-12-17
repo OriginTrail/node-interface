e44ed284f6d1209888c89855c6082aa14021017a
Vladimir Lelicanin <vladimir.lelicanin@origin-trail.com>
2018-12-18 00:03:53 +0100

    Withdraw

--- src/components/Withdraw.vue
+++ src/components/Withdraw.vue
@@ -19,7 +19,7 @@ The maximum amount of TRAC safe to withdraw is shown in the left sidebar. Keep i
                          :step="0.01" :min="0"></el-input-number>
       </el-form-item>
       <el-button @click="startTokenWithdrawal">Tokens Withdrawal</el-button>
-      <!--<el-button @click="withdrawTokens">Withdraw Tokens</el-button>-->
+      <el-button @click="withdrawTokens">Withdraw Tokens</el-button>
     </el-form>
   </div>
 </template>
